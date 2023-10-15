f5d53fe231bf2efea782ed3d54c447bdacc12e70
arjl <arbia.jlassi93@gmail.com>
2023-10-15 19:10:18 +0200

    [feature/unit-tests] add comments to highlight the use of "Given When Then" testing pattern, and update cashWithdrawlWorkflow picture

--- src/test/java/fraj/lab/recruit/test2/atm/ATMTest.java
+++ src/test/java/fraj/lab/recruit/test2/atm/ATMTest.java
@@ -2,6 +2,7 @@ package fraj.lab.recruit.test2.atm;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,24 +27,30 @@ class ATMTest {
     @Test
     void testRunCashWithdrawal_WithInvalidAmount_ReturnsATMTechnicalException() {
 
+        // Given
         int invalidAmount = 0;
-
         when(amountSelectorMock.selectAmount()).thenReturn(invalidAmount);
 
-        assertThrows(ATMTechnicalException.class, atm::runCashWithdrawal);
+        // When
+        Executable executable = () -> atm.runCashWithdrawal();
+
+        // Then
+        assertThrows(ATMTechnicalException.class, executable);
 
     }
 
     @Test
     void testRunCashWithdrawal_WithUnavailableCash_ReturnsCashNotAvailableStatus() throws ATMTechnicalException {
 
+        // Given
         int validAmount = 100;
-
         when(amountSelectorMock.selectAmount()).thenReturn(validAmount);
         when(cashManagerMock.canDeliver(validAmount)).thenReturn(false);
 
+        // When
         ATMStatus atmStatus = atm.runCashWithdrawal();
 
+        // Then
         assertEquals(ATMStatus.CASH_NOT_AVAILABLE, atmStatus);
 
     }
@@ -51,14 +58,16 @@ class ATMTest {
     @Test
     void testRunCashWithdrawal_WithAvailableCashAndFailedPayment_ReturnsPaymentRejectedStatus() throws ATMTechnicalException {
 
+        // Given
         int validAmount = 120;
-
         when(amountSelectorMock.selectAmount()).thenReturn(validAmount);
         when(cashManagerMock.canDeliver(validAmount)).thenReturn(true);
         when(paymentProcessorMock.pay(validAmount)).thenReturn(PaymentStatus.FAILURE);
 
+        // When
         ATMStatus atmStatus = atm.runCashWithdrawal();
 
+        // Then
         assertEquals(ATMStatus.PAYMENT_REJECTED, atmStatus);
 
     }
@@ -66,14 +75,16 @@ class ATMTest {
     @Test
     void testRunCashWithdrawal_WithAvailableCashAndNullPayment_ReturnsPaymentRejectedStatus() throws ATMTechnicalException {
 
+        // Given
         int validAmount = 140;
-
         when(amountSelectorMock.selectAmount()).thenReturn(validAmount);
         when(cashManagerMock.canDeliver(validAmount)).thenReturn(true);
         when(paymentProcessorMock.pay(validAmount)).thenReturn(null);
 
+        // When
         ATMStatus atmStatus = atm.runCashWithdrawal();
 
+        // Then
         assertEquals(ATMStatus.PAYMENT_REJECTED, atmStatus);
 
     }
@@ -81,14 +92,16 @@ class ATMTest {
     @Test
     void testRunCashWithdrawal_WithAvailableCashAndSuccessPayment_ReturnsOperationDoneStatus() throws ATMTechnicalException {
 
+        // Given
         int validAmount = 80;
-
         when(amountSelectorMock.selectAmount()).thenReturn(validAmount);
         when(cashManagerMock.canDeliver(validAmount)).thenReturn(true);
         when(paymentProcessorMock.pay(validAmount)).thenReturn(PaymentStatus.SUCCESS);
 
+        // When
         ATMStatus atmStatus = atm.runCashWithdrawal();
 
+        // Then
         assertEquals(ATMStatus.DONE, atmStatus);
 
     }
